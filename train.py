18b55909eee580c2978ac06a14cbba52e21cce37
Prasad Pamidi <pamidi.dev@gmail.com>
2017-08-15 07:44:57 +0530

    Adding back lenet arch model as well

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Dropout
-from keras.layers import Cropping2D, Convolution2D
+from keras.layers import Cropping2D, Convolution2D, MaxPooling2D
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -62,13 +62,41 @@ def generator(samples, batch_size=32):
 
             yield shuffle(x_train, y_train)
 
-def generate_nvidia_model():
+def add_preprocessing_layers(model):
+    """
+    Apply some common data preprocessing layers to the keras model.
+    """
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+def lenet_arch_model():
+    """
+    Returns the keras model for the LENET architecture.
+    """
+    model = Sequential()
+    add_preprocessing_layers(model)
+
+    model.add(Convolution2D(6, kernel_size=(5, 5), padding="valid", activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(16, kernel_size=(5, 5), padding="valid", activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+
+    model.add(Dense(120, activation='relu'))
+    model.add(Dropout(0.65))
+    model.add(Dense(84, activation='relu'))
+    model.add(Dropout(0.65))
+    model.add(Dense(1))
+
+    return model
+
+def nvidia_arch_model():
     """
     Returns the keras model for the popular NVIDIA architecture.
     """
     model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
-    model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+    add_preprocessing_layers(model)
 
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
@@ -78,11 +106,8 @@ def generate_nvidia_model():
 
     model.add(Flatten())
     model.add(Dense(100))
-    model.add(Dropout(0.65))
     model.add(Dense(50))
-    model.add(Dropout(0.65))
     model.add(Dense(10))
-    model.add(Dropout(0.65))
     model.add(Dense(1))
 
     return model
@@ -105,22 +130,22 @@ data_samples = process_data('data', 0.2)
 train_samples, validation_samples = train_test_split(data_samples, test_size=0.2)
 
 ### Compile and train the model using the generator function
-nvidia_model = generate_nvidia_model()
-nvidia_model.compile(loss='mse', optimizer='adam')
+keras_model = lenet_arch_model()
+keras_model.compile(loss='mse', optimizer='adam')
 
 ### Create seperate generators for training and validation data
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-history_object = nvidia_model.fit_generator(train_generator,
+history_object = keras_model.fit_generator(train_generator,
                                             samples_per_epoch=len(train_samples),
                                             validation_data=validation_generator,
                                             nb_val_samples=len(validation_samples),
-                                            nb_epoch=5,
+                                            nb_epoch=3,
                                             verbose=1)
 
 ### Save the trained model
-nvidia_model.save('model.h5')
+keras_model.save('model.h5')
 
 ### Visualize the model loss over training and validation data
 visualize_model_loss(history_object)
