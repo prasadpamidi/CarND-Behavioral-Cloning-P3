8028d8ca00ce972aca0c1718f57ac700e47da1c2
Prasad Pamidi <pamidi.dev@gmail.com>
2017-08-21 11:51:29 +0530

    Adding threashold to control zero angle count

--- model.py
+++ model.py
@@ -5,13 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout
-from keras.layers import Cropping2D, Convolution2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Dropout, Cropping2D, Convolution2D, MaxPooling2D
+from keras.layers.advanced_activations import ELU
 from keras.callbacks import ModelCheckpoint
 from keras.utils import plot_model
 from keras.optimizers import Adam
 from keras.regularizers import l2
-from keras.layers.advanced_activations import ELU
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -29,7 +28,7 @@ def process_data(directory, correction_factor):
         reader = csv.reader(csv_file)
         for line in reader:
             lines.append(line)
-    
+
     processed_results = []
 
     images_dir = directory + '/IMG/'
@@ -38,12 +37,12 @@ def process_data(directory, correction_factor):
 
     for line in lines:
         steering_center = float(line[3])
-        steering_left = steering_center + correction_factor
-        steering_right = steering_center - correction_factor
+        steering_left = steering_center+correction_factor
+        steering_right = steering_center-correction_factor
 
-        img_center_path = images_dir + line[0].split('/')[-1]
-        img_left_path = images_dir + line[1].split('/')[-1]
-        img_right_path = images_dir + line[2].split('/')[-1]
+        img_center_path = images_dir+line[0].split('/')[-1]
+        img_left_path = images_dir+line[1].split('/')[-1]
+        img_right_path = images_dir+line[2].split('/')[-1]
 
         processed_results.append((img_center_path, steering_center))
         processed_results.append((img_left_path, steering_left))
@@ -56,8 +55,8 @@ def random_brightness_image(image):
     Returns an image with a random degree of brightness.
     """
     dst = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    random_bright = .25 + np.random.uniform()
-    dst[:, :, 2] = dst[:, :, 2] * random_bright
+    random_bright = .25+np.random.uniform()
+    dst[:, :, 2] = dst[:, :, 2]*random_bright
     dst = cv2.cvtColor(dst, cv2.COLOR_HSV2RGB)
     return dst
 
@@ -67,8 +66,8 @@ def random_translate_image(image):
     '''
     rows, cols = image.shape[0], image.shape[1]
 
-    translation_x = 10 * np.random.uniform() - 5
-    translation_y = 10 * np.random.uniform() - 5
+    translation_x = 10*np.random.uniform()-5
+    translation_y = 10*np.random.uniform()-5
     M = np.float32([[1, 0, translation_x], [0, 1, translation_y]])
     return cv2.warpAffine(image, M, (cols, rows))
 
@@ -78,10 +77,10 @@ def random_warp_image(image):
     '''
     rows, cols, _ = image.shape
 
-    rnd_x = np.random.rand(3) - 0.5
-    rnd_x *= cols * 0.065
-    rnd_y = np.random.rand(3) - 0.5
-    rnd_y *= rows * 0.065
+    rnd_x = np.random.rand(3)-0.5
+    rnd_x *= cols*0.065
+    rnd_y = np.random.rand(3)-0.5
+    rnd_y *= rows*0.065
 
     x1 = cols/4
     x2 = 3*cols/4
@@ -108,34 +107,47 @@ def generator(samples, batch_size=32, validation=False):
     Returns a generator for the required images and augmented images from the given set of samples.
     """
     num_samples = len(samples)
+
     while 1:
         samples = shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
+            zero_measurement_count = 0
+            _, unique_rotation_angle_counts = np.unique([x[1] for x in batch_samples],
+                                                        return_counts=True)
+            angles_count_mean = np.mean(unique_rotation_angle_counts)
+            print("Angles count mean for this batch {}".format(angles_count_mean))
             augmented_images, augmented_measurements = [], []
 
             for image_path, measurement in batch_samples:
-                image = cv2.imread(image_path)
-                image = cv2.GaussianBlur(image, (3, 3), 0)
+                if abs(measurement) < 0.1 and zero_measurement_count > angles_count_mean:
+                    print("Zero angle count {} is past the mean angle count for this batch."
+                          .format(zero_measurement_count))
+                    yield shuffle(np.array([]), np.array([]))
+                else:
+                    image = cv2.imread(image_path)
+                    image = cv2.GaussianBlur(image, (3, 3), 0)
 
-                if not validation:
+                    #Converting to RGB format as drive.py input images are given in rgb format
                     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                    image = random_distort_image(image)
 
-                augmented_images.append(image)
-                augmented_measurements.append(measurement)
+                    if not validation:
+                        image = random_distort_image(image)
 
-                # Flip images whose measurements are not close to straight angle
-                if abs(measurement) > 0.3:
+                    augmented_images.append(image)
+                    augmented_measurements.append(measurement)
                     augmented_images.append(cv2.flip(image, 1))
-                    augmented_measurements.append(measurement * -1.0)
+                    augmented_measurements.append(measurement*-1.0)
 
-            x_train = np.array(augmented_images)
-            y_train = np.array(augmented_measurements)
+                    x_train = np.array(augmented_images)
+                    y_train = np.array(augmented_measurements)
 
-            yield shuffle(x_train, y_train)
-            x_train, y_train = [], []
+                    if abs(measurement) < 0.1:
+                        zero_measurement_count += 1
+
+                    yield shuffle(x_train, y_train)
+
+                x_train, y_train = [], []
 
 def add_preprocessing_layers(model):
     """
@@ -151,16 +163,18 @@ def lenet_arch_model():
     model = Sequential()
     add_preprocessing_layers(model)
 
-    model.add(Convolution2D(6, kernel_size=(5, 5), padding="valid", activation='relu'))
+    model.add(Convolution2D(6, kernel_size=(5, 5), padding="valid",
+                            activation='relu', kernel_regularizer=l2(0.001)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Convolution2D(16, kernel_size=(5, 5), padding="valid", activation='relu'))
+    model.add(Convolution2D(16, kernel_size=(5, 5), padding="valid",
+                            activation='relu', kernel_regularizer=l2(0.001)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
 
-    model.add(Dense(120, activation='relu'))
+    model.add(Dense(120, activation='relu', kernel_regularizer=l2(0.001)))
     model.add(Dropout(0.65))
-    model.add(Dense(84, activation='relu'))
+    model.add(Dense(84, activation='relu', kernel_regularizer=l2(0.001)))
     model.add(Dropout(0.65))
     model.add(Dense(1))
 
@@ -221,9 +235,9 @@ def keras_model_callbacks():
                             mode='max')]
 
 ### Splitting samples and creating generators.
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 
-data_samples = process_data('data', 0.2)
+data_samples = process_data('data', 0.25)
 train_samples, validation_samples = train_test_split(data_samples, test_size=0.2)
 
 ### Create seperate generators for training and validation data
@@ -240,22 +254,25 @@ else:
     print("No prior model checkpoints exist")
 
 ### Compile and train the model using the generator function
-keras_model.compile(loss='mse', optimizer=Adam(lr=1e-4), metrics=['accuracy'])
+keras_model.compile(loss='mse', optimizer=Adam(lr=1e-5), metrics=['accuracy'])
 
 history_object = keras_model.fit_generator(train_generator,
-                                           steps_per_epoch=int(np.floor((len(train_samples))/BATCH_SIZE)*BATCH_SIZE),
+                                           steps_per_epoch=int(np.floor((len(train_samples))
+                                                                        /BATCH_SIZE)*BATCH_SIZE),
                                            validation_data=validation_generator,
-                                           validation_steps=int(np.floor((len(validation_samples))/BATCH_SIZE)*BATCH_SIZE),
-                                           epochs=5,
+                                           validation_steps=int(np.floor((len(validation_samples))
+                                                                         /BATCH_SIZE)*BATCH_SIZE),
+                                           epochs=7,
                                            verbose=1,
                                            callbacks=keras_model_callbacks())
 keras_model.summary()
 
+keras_model.save('model.h5')
+
 ### Generate visualiaztion of the entire model
-plot_model(keras_model, to_file='model.png', show_shapes=True)
+#plot_model(keras_model, to_file='model.png', show_shapes=True)
 
 ### Save the trained model
-keras_model.save('model.h5')
 
 ### Visualize the model loss over training and validation data - NOT NEEDED FOR EC2
 #visualize_model_loss(history_object)
